48a03ce4f3227a95d8173304f91a755745988b1a
ivnard <ivan.nardini@sas.com>
2020-08-07 14:48:02 +0200

    Start JavaTutorial module

---
+++ 1_JavaTutorial/4_Challenge_Methods/src/com/ivnard/Main.java
@@ -0,0 +1,37 @@
+package com.ivnard;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String PLAYERNAME = "Ivan";
+        DisplayHighScorePosition(PLAYERNAME, 1500);
+        CalculateHighScorePosition(1500);
+        DisplayHighScorePosition(PLAYERNAME, 900);
+        CalculateHighScorePosition(900);
+        DisplayHighScorePosition(PLAYERNAME, 400);
+        CalculateHighScorePosition(400);
+        DisplayHighScorePosition(PLAYERNAME, 50);
+        CalculateHighScorePosition(50);
+    }
+
+    // Create a method DisplayHighScorePosition
+    // with player name parameter and high score table parameter
+    public static void DisplayHighScorePosition(String PlayerName, int Score) {
+        System.out.println(PlayerName + " managed to get into position " + Score + " on the high score table ");
+    }
+
+    // Create a method CalculateHighScorePosition
+    // with Score parameter only
+    public static int CalculateHighScorePosition(int Score) {
+        if (Score > 1000) {
+            return 1;
+        } else if (Score > 500 && Score < 1000) {
+            return 2;
+        } else if (Score > 100 && Score < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
