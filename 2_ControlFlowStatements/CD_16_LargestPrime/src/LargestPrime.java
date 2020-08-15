2f3e5e96e533b2df3b83e3328ab366877450650a
ivnard <ivan.nardini@sas.com>
2020-08-15 17:03:18 +0200

    Control flow module

---
+++ 2_ControlFlowStatements/CD_16_LargestPrime/src/LargestPrime.java
@@ -0,0 +1,10 @@
+public class LargestPrime {
+    //Write a method named getLargestPrime with one parameter of type int named number.
+    //If the number is negative or does not have any prime numbers, the method should return -1 to indicate an invalid value.
+    //The method should calculate the largest prime factor of a given number and return it.
+
+    public static int getLargestPrime(int number){
+
+    }
+
+}
